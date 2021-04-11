d4d9bd245ca44bceae8abdd7350fa04ce55c37d6
Gabriel Andrade <gabriel.santana@gmail.com>
2021-04-11 05:03:13 -0300

    Atualizando ciclo de vida

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 import { View,Text } from 'react-native'
 import AnimatedSpring from './src/components/AnimatedSpring'
+import {Timer} from  'react-native-animated-timer';
 
 export default function App() {
+
+
+const [counter, setCounter] = useState(0);
+const r = useRef(null);
+r.current = { counter, setCounter };
+useEffect(
+  () => {
+    const id = setInterval(() => {
+      if(r.current.counter >= 10){
+        clearInterval(id);
+      } else {
+        r.current.setCounter(r.current.counter + 1);
+
+      }
+  
+      console.log(r.current.counter/10);
+     
+    }, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  },
+  [] // empty dependency array
+);
+
  return (
   <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-    <AnimatedSpring value={0.3} />
+    <AnimatedSpring value={counter/10} />
+
+   
    </View>
   );
 }
